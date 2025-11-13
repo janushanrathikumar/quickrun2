411eab0c5af5efb155443eda85c675f65051830a
RathikumarJanushan <janushanr17@gmail.com>
2025-11-13 10:58:56 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/quickrun2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quickrun2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:quickrun2/firebase_options.dart';
+import 'package:quickrun2/view/admin/adminhome_screen.dart';
+
+import 'package:quickrun2/view/login/welcome_view.dart';
+import 'package:quickrun2/view/on_boarding/startup_view.dart';
+import 'package:quickrun2/view/user/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Initialize Firebase
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Metropolis",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const StartupView(),
+        '/home': (context) => const HomeScreen(),
+        '/login': (context) => const WelcomeView(),
+        '/adminhome': (context) => const AdminhomeomeScreen(),
+        // Define other routes here
+      },
+    );
+  }
+}
